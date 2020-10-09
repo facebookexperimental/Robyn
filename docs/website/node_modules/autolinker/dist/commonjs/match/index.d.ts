34a7d67637193f64fb1ae5be01663e8bface352d
Leonel Sentana <leonelsentana@fb.com>
2020-10-09 16:00:18 +0200

    Initial commit

--- docs/website/node_modules/autolinker/dist/commonjs/match/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './email-match';
-export * from './hashtag-match';
-export * from './match';
-export * from './mention-match';
-export * from './phone-match';
-export * from './url-match';
