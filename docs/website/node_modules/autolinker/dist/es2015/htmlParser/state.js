1c92d4be8145844a5054e6c1aab82c3c0da3f4da
Leonel Sentana <leonelsentana@fb.com>
2020-11-05 13:35:36 +0000

    Adding new website

---
+++ docs/website/node_modules/autolinker/dist/es2015/htmlParser/state.js
@@ -0,0 +1,3 @@
+
+
+//# sourceMappingURL=state.js.map
---
+++ docs/website/node_modules/autolinker/dist/es2015/index.js
@@ -0,0 +1,13 @@
+// WARNING: This file is modified a bit when it is compiled into index.js in 
+// order to support nodejs interoperability with require('autolinker') directly. 
+// This is done by the buildSrcFixCommonJsIndexTask() function in the gulpfile. 
+// See that function for more details.
+export { default } from './autolinker';
+export { default as Autolinker } from './autolinker';
+export * from './autolinker';
+export * from './anchor-tag-builder';
+export * from './html-tag';
+export * from './match/index';
+export * from './matcher/index';
+
+//# sourceMappingURL=index.js.map
