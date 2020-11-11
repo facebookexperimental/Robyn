783f2d2c04f9cc6e62dbe8e2776660071754b429
Leonel Sentana <leonelsentana@fb.com>
2020-11-11 11:24:52 +0000

    Adding new website

--- docs/.docusaurus/routes.js
+++ docs/.docusaurus/routes.js
@@ -3,9 +3,13 @@ import React from 'react';
 import ComponentCreator from '@docusaurus/ComponentCreator';
 export default [
 {
-<<<<<<< HEAD
   path: '/Robyn/',
   component: ComponentCreator('/Robyn/','0b2'),
+  exact: true,
+},
+{
+  path: '/Robyn/__docusaurus/debug',
+  component: ComponentCreator('/Robyn/__docusaurus/debug','2b8'),
   exact: true,
 },
 {
@@ -111,120 +115,6 @@ export default [
 {
   path: '/Robyn/docs/doc9',
   component: ComponentCreator('/Robyn/docs/doc9','d37'),
-=======
-  path: '/',
-  component: ComponentCreator('/','deb'),
-  exact: true,
-},
-{
-  path: '/__docusaurus/debug',
-  component: ComponentCreator('/__docusaurus/debug','fdd'),
-  exact: true,
-},
-{
-  path: '/blog',
-  component: ComponentCreator('/blog','728'),
-  exact: true,
-},
-{
-  path: '/blog/hello-world',
-  component: ComponentCreator('/blog/hello-world','798'),
-  exact: true,
-},
-{
-  path: '/blog/hola',
-  component: ComponentCreator('/blog/hola','08d'),
-  exact: true,
-},
-{
-  path: '/blog/tags',
-  component: ComponentCreator('/blog/tags','3a8'),
-  exact: true,
-},
-{
-  path: '/blog/tags/docusaurus',
-  component: ComponentCreator('/blog/tags/docusaurus','917'),
-  exact: true,
-},
-{
-  path: '/blog/tags/facebook',
-  component: ComponentCreator('/blog/tags/facebook','c2b'),
-  exact: true,
-},
-{
-  path: '/blog/tags/hello',
-  component: ComponentCreator('/blog/tags/hello','c12'),
-  exact: true,
-},
-{
-  path: '/blog/tags/hola',
-  component: ComponentCreator('/blog/tags/hola','084'),
-  exact: true,
-},
-{
-  path: '/blog/welcome',
-  component: ComponentCreator('/blog/welcome','cb0'),
-  exact: true,
-},
-{
-  path: '/docs',
-  component: ComponentCreator('/docs','19a'),
-  
-  routes: [
-{
-  path: '/docs/',
-  component: ComponentCreator('/docs/','f0b'),
-  exact: true,
-},
-{
-  path: '/docs/doc1',
-  component: ComponentCreator('/docs/doc1','392'),
-  exact: true,
-},
-{
-  path: '/docs/doc10',
-  component: ComponentCreator('/docs/doc10','120'),
-  exact: true,
-},
-{
-  path: '/docs/doc3',
-  component: ComponentCreator('/docs/doc3','e02'),
-  exact: true,
-},
-{
-  path: '/docs/doc4',
-  component: ComponentCreator('/docs/doc4','50f'),
-  exact: true,
-},
-{
-  path: '/docs/doc5',
-  component: ComponentCreator('/docs/doc5','b2b'),
-  exact: true,
-},
-{
-  path: '/docs/doc6',
-  component: ComponentCreator('/docs/doc6','b6d'),
-  exact: true,
-},
-{
-  path: '/docs/doc7',
-  component: ComponentCreator('/docs/doc7','ad0'),
-  exact: true,
-},
-{
-  path: '/docs/doc8',
-  component: ComponentCreator('/docs/doc8','728'),
-  exact: true,
-},
-{
-  path: '/docs/doc9',
-  component: ComponentCreator('/docs/doc9','1a8'),
-  exact: true,
-},
-{
-  path: '/docs/mdx',
-  component: ComponentCreator('/docs/mdx','955'),
->>>>>>> 88a95a753303f58e6c9f4b7b4e8c8c7a4488fdec
   exact: true,
 },
 ]
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,6 @@
  */
 
 module.exports = {
-<<<<<<< HEAD
   title: 'Robyn',
   tagline: 'Robyn is a Marketing Mix Modeling (MMM) code. It can be used to build end-to-end time series regression models and as an econometrics code library. Automated, built for very large data sets, and is suitable for digital and complex consumer behaviour',
   url: 'https://facebookexperimental.github.io/Robyn/',
@@ -34,34 +33,6 @@ module.exports = {
         // Please keep GitHub link to the right for consistency.
         {
           href: 'https://github.com/facebookexperimental/Robyn',
-=======
-  title: 'MMM for all',
-  tagline: 'Democratizing marketing mix models',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'MMM for all', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'MMM for all',
-      logo: {
-        alt: 'My Facebook Project Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        // Please keep GitHub link to the right for consistency.
-        {
-          href: 'https://github.com/facebook/docusaurus',
->>>>>>> 88a95a753303f58e6c9f4b7b4e8c8c7a4488fdec
           label: 'GitHub',
           position: 'right',
         },
@@ -71,53 +42,11 @@ module.exports = {
       style: 'dark',
       links: [
         {
-<<<<<<< HEAD
           title: 'More',
           items: [
             {
               label: 'GitHub',
               href: 'https://github.com/facebookexperimental/Robyn',
-=======
-          title: 'Learn',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
->>>>>>> 88a95a753303f58e6c9f4b7b4e8c8c7a4488fdec
             },
           ],
         },
@@ -155,11 +84,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-<<<<<<< HEAD
             'https://github.com/facebookexperimental/Robyn',
-=======
-            'https://github.com/facebook/docusaurus/edit/master/website/',
->>>>>>> 88a95a753303f58e6c9f4b7b4e8c8c7a4488fdec
         },
         blog: {
           showReadingTime: true,
