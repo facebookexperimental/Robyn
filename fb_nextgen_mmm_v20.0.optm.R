12382299ae1abfcd57108449c536af549a1f16e3
Leonelsentana <70534991+Leonelsentana@users.noreply.github.com>
2021-02-12 13:03:11 +0100

    Add files via upload

---
+++ fb_nextgen_mmm_v20.0.optm.R
@@ -0,0 +1,341 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+################################################################
+#### Define optimiser function
+
+f.optimiser <- function(optim_algo = "MMA_AUGLAG"
+                        ,expected_spend = NULL
+                        ,expected_spend_days = NULL
+                        ,channel_constr_low = 0.5
+                        ,channel_constr_up = 2
+                        ,scenario = "max_historical_response"
+                        ,maxeval = 100000
+                        ,constr_mode = "eq") {
+  
+  if (exists("best_model")) {
+    
+    ## check input parameters
+    if (any(channel_constr_low <0.01) | any(channel_constr_low >1)) {stop("channel_constr_low must be between 0.01 and 1")}
+    if (any(channel_constr_up <1) | any(channel_constr_up >5)) {stop("channel_constr_up must be between 1-5")}
+    if (!(scenario %in% c("max_historical_response", "max_response_expected_spend"))) {
+      stop("scenario must be 'max_historical_response', 'max_response_expected_spend'")
+    }
+    
+    if (length(channel_constr_up)!=1) {
+      if (length(channel_constr_low)!= length(set_mediaVarName) | length(channel_constr_up)!= length(set_mediaVarName)) {
+        stop("channel_constr_low & channel_constr_up have to contain either only 1 value or have same length as set_mediaVarName")
+      }
+    }
+    
+    ## get dt 
+    dt_bestHyperParam <- best_model$resultCollect$resultHypParam
+    dt_bestCoef <- best_model$resultCollect$xDecompAgg
+    dt_optim <- copy(dt_mod)
+    
+    ## get filter for channels mmm coef reduced to 0
+    dt_coef <- best_model$resultCollect$xDecompAgg[rn %in% set_mediaVarName, .(rn, coef)]
+    dt_coefSorted <- dt_coef[order(rn)]
+    coefSelectorSorted <- dt_coefSorted[, coef>0]
+    
+    ## filter and sort all variables by name that is essential for the apply function later
+    channelNames <- sort(set_mediaVarName)
+    channelNames <- channelNames[coefSelectorSorted]
+    if(!all(coefSelectorSorted)) {
+      message(paste(setdiff(set_mediaVarName, channelNames), collapse = ", "), " are excluded in optimiser because their coeffients are 0")
+    }
+    
+    dt_bestHyperParam <- dt_bestHyperParam[, .SD, .SDcols = na.omit(str_extract(names(dt_bestHyperParam),paste(paste0(channelNames,".*"),collapse = "|")))]
+    setcolorder(dt_bestHyperParam, sort(names(dt_bestHyperParam)))
+    
+    dt_optim <- dt_optim[, channelNames, with = F]
+    setcolorder(dt_optim, sort(names(dt_optim)))
+    
+    dt_optimCost <- copy(dt_input)
+    dt_optimCost <- dt_optimCost[, set_mediaSpendName, with = F]
+    names(dt_optimCost) <- set_mediaVarName
+    dt_optimCost <- dt_optimCost[, channelNames, with=F]
+    setcolorder(dt_optimCost, channelNames)
+    
+    dt_bestCoef <- dt_bestCoef[order(rn)][rn %in% channelNames]
+    
+    costMultiplierVec <- mediaCostFactor[channelNames]
+    
+    if(any(costSelector)) {
+      dt_modNLS <- merge(data.table(channel=channelNames), modNLSCollect, all.x = T, by = "channel")
+      vmaxVec <- dt_modNLS[order(channel)][, Vmax]
+      names(vmaxVec) <- channelNames
+      kmVec <- dt_modNLS[order(channel)][, Km]
+      names(kmVec) <- channelNames
+    } else {
+      vmaxVec <- rep(0, length(channelNames))
+      kmVec <- rep(0, length(channelNames))
+    }
+    
+    names(costSelector) <- set_mediaVarName
+    costSelectorSorted <- costSelector[order(set_mediaVarName)]
+    costSelectorSorted <- costSelectorSorted[coefSelectorSorted]
+    
+    channelConstrLowSorted <- channel_constr_low[order(set_mediaVarName)][coefSelectorSorted]
+    channelConstrUpSorted <- channel_constr_up[order(set_mediaVarName)][coefSelectorSorted]
+
+    ## get adstock parameters for each channel
+    if (adstock == "geometric") {
+      getAdstockHypPar <- unlist(dt_bestHyperParam[, .SD, .SDcols = na.omit(str_extract(names(dt_bestHyperParam),".*_thetas"))])
+    } else if (adstock == "weibull") {
+      getAdstockHypPar <- unlist(dt_bestHyperParam[, .SD, .SDcols = na.omit(str_extract(names(dt_bestHyperParam),".*_shapes|.*_scales"))])
+    }
+    
+    ## get hill parameters for each channel
+    hillHypParVec <- unlist(dt_bestHyperParam[, .SD, .SDcols = na.omit(str_extract(names(dt_bestHyperParam),".*_alphas|.*_gammas"))])
+    alphas <- hillHypParVec[str_which(names(hillHypParVec), "_alphas")]
+    
+    adstockTrans <- mapply(function(chnl) {
+      
+      alpha <- hillHypParVec[str_which(names(hillHypParVec), paste0(chnl, "_alphas"))]
+      gamma <- hillHypParVec[str_which(names(hillHypParVec), paste0(chnl, "_gammas"))]
+      chnRaw <- unlist(dt_optim[, chnl, with =F])
+      
+      if (adstock == "geometric") {
+        chnAdstocked <- f.transformation(chnRaw
+                                      ,theta = getAdstockHypPar[str_which(names(getAdstockHypPar), chnl)]
+                                      ,alpha = alpha
+                                      ,gamma = gamma
+                                      ,alternative = adstock
+                                      ,stage = 2)
+      } else if (adstock == "weibull") {
+        chnAdstocked <- f.transformation(chnRaw
+                                      ,shape = getAdstockHypPar[str_which(names(getAdstockHypPar), paste0(chnl, "_shapes"))]
+                                      ,scale = getAdstockHypPar[str_which(names(getAdstockHypPar), paste0(chnl, "_scales"))]
+                                      ,alpha = alpha
+                                      ,gamma = gamma
+                                      ,alternative = adstock
+                                      ,stage = 2)
+      }
+      
+      gammaTrans <- round(quantile(seq(range(chnAdstocked)[1],range(chnAdstocked)[2], length.out = 100), gamma),4)
+      names(gammaTrans) <- NULL
+      
+      return(list(gammaTrans=gammaTrans, chnAdstocked=chnAdstocked))
+      
+    }, chnl = channelNames, SIMPLIFY = F)
+    
+    gammaTrans <- sapply(adstockTrans, function(x) x[1][["gammaTrans"]])
+    chnAdstocked <- as.data.table(sapply(mapply(function(lst, n) data.table(n=lst[2][["chnAdstocked"]]), lst = adstockTrans, n = names(adstockTrans)), cbind))
+    names(chnAdstocked) <- channelNames
+    adstockMultiplierVec <- colSums(chnAdstocked) / colSums(dt_optimCost)
+    
+    coefs <- dt_bestCoef[,coef]; names(coefs) <- dt_bestCoef[,rn]
+    
+    ## build evaluation funciton
+    eval_f <- function(X) {
+      return(
+        list(
+          "objective" = -sum(
+            mapply(function(x, costMultiplier, adstockMultiplier, coeff
+                            , alpha, gammaTran
+                            , chnName, vmax, km, criteria) {
+              # apply Michaelis Menten model to scale spend to reach
+              if (criteria) {
+                xScaled <- vmax * x / (km + x)
+              } else {
+                xScaled <- x
+              }
+              
+              # adstock scales
+              xAdstocked <- xScaled*adstockMultiplier
+              
+              # hill transformation
+              #xOut <- coeff * sum( (1 + gammaTran**alpha / (x/costMultiplier*adstockMultiplier) **alpha)**-1) 
+              xOut <- coeff * sum( (1 + gammaTran**alpha / xAdstocked **alpha)**-1)
+              
+              return(xOut)
+            }, x=X, costMultiplier = costMultiplierVec, adstockMultiplier=adstockMultiplierVec, coeff = coefs
+            , alpha = alphas, gammaTran = gammaTrans
+            , chnName = channelNames
+            , vmax = vmaxVec, km = kmVec, criteria = costSelectorSorted
+            , SIMPLIFY = T)
+          ),
+          
+          "gradient" = c(
+            mapply(function(x, costMultiplier, adstockMultiplier, coeff
+                            , alpha, gammaTran
+                            , chnName, vmax, km, criteria) {
+              # apply Michaelis Menten model to scale spend to reach
+              if (criteria) {
+                xScaled <- vmax * x / (km + x)
+              } else {
+                xScaled <- x
+              }
+              
+              # adstock scales
+              xAdstocked <- xScaled*adstockMultiplier
+              
+              #xOut <- -coeff * sum((alpha * (gammaTran**alpha) * ((x/costMultiplier*adstockMultiplier)**(alpha-1))) / ((x/costMultiplierVec*adstockMultiplierVec)**alpha + gammaTran**alpha)**2)
+              xOut <- -coeff * sum((alpha * (gammaTran**alpha) * (xAdstocked**(alpha-1))) / (xAdstocked**alpha + gammaTran**alpha)**2)
+              
+              return(xOut)
+            }, x=X, costMultiplier = costMultiplierVec, adstockMultiplier=adstockMultiplierVec, coeff = coefs
+            , alpha = alphas, gammaTran = gammaTrans
+            , chnName = channelNames
+            , vmax = vmaxVec, km = kmVec, criteria = costSelectorSorted
+            , SIMPLIFY = T)
+          ), # https://www.derivative-calculator.net/ on the objective function 1/(1+gamma^alpha / x^alpha)
+          
+          "objective.channel" =
+            mapply(function(x, costMultiplier, adstockMultiplier, coeff
+                            , alpha, gammaTran
+                            , chnName, vmax, km, criteria) {
+              
+              # apply Michaelis Menten model to scale spend to reach
+              if (criteria) {
+                xScaled <- vmax * x / (km + x)
+              } else {
+                xScaled <- x
+              }
+              
+              # adstock scales
+              xAdstocked <- xScaled*adstockMultiplier
+              
+              #xOut <- -coeff * sum( (1 + gammaTran**alpha / (x/costMultiplier*adstockMultiplier) **alpha)**-1)
+              xOut <- -coeff * sum( (1 + gammaTran**alpha / xAdstocked **alpha)**-1)
+              
+              return(xOut)
+            }, x=X, costMultiplier = costMultiplierVec, adstockMultiplier=adstockMultiplierVec, coeff = coefs
+            , alpha = alphas, gammaTran = gammaTrans
+            , chnName = channelNames
+            , vmax = vmaxVec, km = kmVec, criteria = costSelectorSorted
+            , SIMPLIFY = T)
+          
+        ))}
+    
+    #eval_f(c(1,1))
+    
+    ## build contraints function with scenarios
+    nPeriod <- nrow(dt_optimCost)
+    if (scenario == "max_historical_response") {
+      expected_spend <- sum(dt_optimCost)
+      expSpendUnitTotal <- expected_spend / nPeriod
+      
+    } else if (scenario == "max_response_expected_spend") {
+      
+      if (any(is.null(expected_spend), is.null(expected_spend_days))) {
+        stop("when scenario = 'max_response_expected_spend', expected_spend and expected_spend_days must be provided")
+      }
+      expSpendUnitTotal <- expected_spend / (expected_spend_days / dayInterval)
+    }
+    
+    histSpend <- colSums(dt_optimCost)
+    histSpendTotal <- sum(histSpend)
+    histSpendUnitTotal <- histSpendTotal/ nPeriod
+    histSpendShare <- histSpend / histSpendTotal
+    histSpendUnit <- histSpendUnitTotal * histSpendShare
+    histResponseUnit <- -eval_f(histSpendUnit)[["objective.channel"]]
+    
+    # expSpendUnit <- histSpendShare * expSpendUnitTotal
+    # expResponseUnit <- -eval_f(expSpendUnit)[["objective.channel"]]
+    
+    eval_g_eq <- function(X) {
+      constr <- sum(X) - expSpendUnitTotal
+      grad <- rep(1, length(X))
+      return( list("constraints" = constr
+                   ,"jacobian" = grad))
+    }
+    
+    eval_g_ineq <- function(X) {
+      constr <- sum(X) - expSpendUnitTotal
+      grad <- rep(1, length(X))
+      return( list("constraints" = constr
+                   ,"jacobian" = grad))
+    }
+    
+    ## set initial values and bounds
+    lb <- histSpendUnit * channelConstrLowSorted
+    ub <- histSpendUnit * channelConstrUpSorted
+    x0 <- lb
+    
+    ## set optim options
+    if (optim_algo == "MMA_AUGLAG") {
+      local_opts <- list( "algorithm" = "NLOPT_LD_MMA",
+                          "xtol_rel" = 1.0e-10 )
+      opts <- list( "algorithm" = "NLOPT_LD_AUGLAG",
+                    "xtol_rel" = 1.0e-10,
+                    "maxeval" = maxeval,
+                    "local_opts" = local_opts )
+    }
+    
+    ## run optim
+    if (constr_mode  == "eq") {
+      nlsMod <- nloptr( x0=x0,
+                        eval_f=eval_f,
+                        lb=lb,
+                        ub=ub,
+                        #eval_g_ineq=eval_g_ineq,
+                        eval_g_eq=eval_g_eq,
+                        opts=opts)
+    } else if (constr_mode  == "ineq") {
+      nlsMod <- nloptr( x0=x0,
+                        eval_f=eval_f,
+                        lb=lb,
+                        ub=ub,
+                        eval_g_ineq=eval_g_ineq,
+                        #eval_g_eq=eval_g_eq,
+                        opts=opts)
+    }
+    
+    #print(nlsMod)
+    
+    
+  } else {
+    stop("must run f.mmm.out function first to get output best_model")
+  }
+  
+  ## collect output 
+  
+  dt_bestModel <- best_model$resultCollect$xDecompAgg[rn %in% channelNames, .(rn, spend, xDecompAgg, roi)][order(rn)]
+  
+  dt_optimOut <- data.table(
+    channels = channelNames
+    ,histSpend = histSpend
+    ,histSpendTotal = histSpendTotal
+    ,initSpendUnitTotal = histSpendUnitTotal
+    ,initSpendUnit = histSpendUnit
+    ,initSpendShare = histSpendShare
+    # ,histResponse = dt_bestModel$xDecompAgg
+    # ,histResponseTotal = sum(dt_bestModel$xDecompAgg)
+    # ,histResponseUnit = dt_bestModel$xDecompAgg/nPeriod
+    # ,histResponseUnitTotal = sum(dt_bestModel$xDecompAgg/nPeriod)
+    ,initResponseUnit = histResponseUnit
+    ,initResponseUnitTotal = sum(histResponseUnit)
+    ,initRoiUnit = histResponseUnit/histSpendUnit
+    ,expSpendTotal = expected_spend
+    ,expSpendUnitTotal = expSpendUnitTotal
+    ,expSpendUnitDelta = expSpendUnitTotal/histSpendUnitTotal-1
+    #,expSpendUnit = expSpendUnit
+    # ,expResponseUnit = expResponseUnit
+    # ,expResponseUnitTotal = sum(expResponseUnit)
+    # ,expRoiUnit = expResponseUnit / expSpendUnit
+    ,optmSpendUnit = nlsMod$solution
+    ,optmSpendUnitDelta = (nlsMod$solution / histSpendUnit -1)
+    ,optmSpendUnitTotal = sum(nlsMod$solution)
+    ,optmSpendUnitTotalDelta = sum(nlsMod$solution)/histSpendUnitTotal-1
+    ,optmSpendShareUnit = nlsMod$solution / sum(nlsMod$solution)
+    ,optmResponseUnit = -eval_f(nlsMod$solution)[["objective.channel"]]
+    ,optmResponseUnitTotal = sum(-eval_f(nlsMod$solution)[["objective.channel"]])
+    ,optmRoiUnit = -eval_f(nlsMod$solution)[["objective.channel"]] / nlsMod$solution
+    ,optmResponseUnitLift = (-eval_f(nlsMod$solution)[["objective.channel"]] / histResponseUnit) -1
+  )
+  
+  dt_optimOut[, optmResponseUnitTotalLift:= (optmResponseUnitTotal / initResponseUnitTotal) -1]
+  
+  return(list(dt_optimOut=dt_optimOut, nlsMod=nlsMod))
+}
+
+
+#lib https://cran.r-project.org/web/packages/nloptr/nloptr.pdf non linear function with equal and unequal constraints + bounds
+#find gradient of a function https://socratic.org/questions/how-do-you-find-the-gradient-of-a-function-at-a-given-point
+
+# # lib https://cran.r-project.org/web/packages/nloptr/nloptr.pdf
+# # find gradient of a function https://socratic.org/questions/how-do-you-find-the-gradient-of-a-function-at-a-given-point
