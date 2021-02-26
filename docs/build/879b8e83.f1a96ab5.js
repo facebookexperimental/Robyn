0ec1f5595d75c0fb561f9e11aedf9fb75b5854eb
Yangshun Tay <tay.yang.shun@gmail.com>
2021-02-26 23:19:22 +0800

    website: remove redundant code

--- docs/build/879b8e83.f1a96ab5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{72:function(o){o.exports=JSON.parse('{"allTagsPath":"/Robyn/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/Robyn/blog/tags/facebook"}')}}]);
