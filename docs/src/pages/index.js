a68240d35863182569ac4ee4c4cafdcbbc9e2b25
Leonel Sentana <leonelsentana@fb.com>
2021-04-06 16:43:46 +0200

    website for Robyn v2.0 updates

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -14,71 +14,82 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
+
 const features = [
   {
-    title: <>Advanced</>,
+    title: <>Reduces human bias</>,
     imageUrl: 'img/take_control.svg',
     description: (
       <>
-      <ul>
-      <li> Automated code, making it significantly faster to run </li>
-      <li> Can be calibrated and validated using real world experiments</li>
-      <li> Fully customisable adstock to suit your business</li>
-      <li> Automated seasonality and richer external variables using Facebook code ‘Prophet’, increasing interpretability and model fit</li>
-      <li> Uses Ridge Regression to solve for multicollinearity</li>
-      <li> Built to manage large data sets and numbers of variables making it ideal for digital marketing and complex consumer behaviour</li>
-    </ul>
+        <ul>
+          <li> Automated hyperparameter optimization with evolutionary algorithms from Facebook's AI library <a href="https://facebookresearch.github.io/nevergrad">Nevergrad</a></li>
+          <li> Ridge regression with cross-validation to regularize multi-collinearity and prevent overfitting</li>
+          <li> Facebook's <a href="https://facebook.github.io/prophet/">Prophet</a> library to automatically decompose the trend, seasonality and holidays patterns</li>
+        </ul>
       </>
     ),
   },
   {
-    title: <>Controllable and Scalable</>,
+    title: <>Aligns with the ground-truth</>,
     imageUrl: 'img/calibrate.svg',
     description: (
       <>
-    <ul>
-      <li> Standardised and stable code to limit analyst bias and subjectivity, making models scaleable and transferable</li>
-      <li> Fully customisable to accommodate multiple unique variables that matter your business</li>
-      <li> Increase the number of models and frequency as faster to run and automated</li>
-      <li> Model all of the outputs that matter to your business</li>
-    </ul>
+        <ul>
+          <li>
+            {' '}
+            It calibrates models based on ground-truth methodologies (Geo-based, Facebook lift, MTA, etc.)
+          </li>
+          <li>
+            {' '}
+            Facebook <a href="https://facebookresearch.github.io/nevergrad">Nevergrad</a>'s multi-objective optimization minimizing the error between MMM prediction and ground-truth
+          </li>
+        </ul>
       </>
     ),
   },
   {
-    title: <>Actionable</>,
+    title: <>Enables actionable decision making</>,
     imageUrl: 'img/focus_on_what matters.svg',
     description: (
       <>
-      <ul>
-      <li> No need to wait until your campaigns have finished. Faster modeling allows for inflight campaign optimization.</li>
-      <li> Continuous modeling helps you to understand the performance of your marketing in almost real time.</li>
-      <li> Includes integrated marketing budget optimizer with the ability to apply custom restraints</li>
-      <li> Not dependent on Facebook advertising but can connect to measurement products in Facebook Ads Manager for granular insights</li>
-    </ul>
+        <ul>
+          <li>
+            {' '}
+            Budget allocator using a gradient-based constrained non-linear solver to maximize the outcome by reallocating budgets
+          </li>
+          <li>
+            {' '}
+            Enables frequent modeling outcomes due to stronger automation
+          </li>
+          <li>
+            {' '}
+            Allows intuitive model comparisons via automatically generated model one-pagers
+          </li>
+        </ul>
       </>
     ),
   },
   {
-    description: (
-      <>
-      </>
-    ),
+    description: <></>,
   },
   {
     title: <>Privacy by Design</>,
     description: (
       <>
-      <ul>
-      <li> Privacy safe with no requirement for PII or Individual log level data</li>
-      <li> Not dependent on Cookies or Pixel data</li>
-      </ul>
+        <ul>
+          <li>
+            {' '}
+            Privacy safe with no requirement for PII or Individual log level
+            data
+          </li>
+          <li> Not dependent on Cookies or Pixel data</li>
+        </ul>
       </>
     ),
-  }
+  },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
@@ -95,11 +106,12 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
@@ -110,18 +122,33 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('docs/')}
+            >
               Get Started
             </Link>
           </div>
         </div>
       </header>
       <main>
+        <div className="container padding-top--lg">
+          <div className="row">
+            <div className="col col--6 col--offset-3">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/8SyKRpsXn44"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              />
+            </div>
+          </div>
+        </div>
         {features && features.length > 0 && (
           <section className={styles.features}>
             <div className="container">
               <div className="row">
-                {features.map(({title, imageUrl, description}) => (
+                {features.map(({ title, imageUrl, description }) => (
                   <Feature
                     key={title}
                     title={title}
