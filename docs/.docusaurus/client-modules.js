ad26a3fffb6fa1b8a882149f91c0af75817daa3c
🖖 Randall Hunt🖖 <ranman@fb.com>
2021-04-07 15:30:52 -0700

    move docs to website, upgrade docusaurus version

--- docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/leonelsentana/Robyn/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/leonelsentana/Robyn/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/leonelsentana/Robyn/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/leonelsentana/Robyn/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/leonelsentana/Robyn/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/leonelsentana/Robyn/docs/src/css/custom.css"),
-];
