1140a3f060a321bcf6a9f976d5563930d1c0ad6b
Gufeng Zhou <gufengzhou@fb.com>
2021-10-22 17:14:07 +0200

    deploy weibull_pdf function with lagged effect
    - added weibull_pdf as option into adstock_weibull() and adapted all related functions
    - improved documentation and readme

---
+++ demo/debug.R
@@ -0,0 +1,11 @@
+devtools::load_all()
+
+## debug robyn_mmm
+# prep input param
+hyper_collect = InputCollect$hyperparameters
+iterations = InputCollect$iterations
+lambda.n = 100
+lambda_control = 1
+lambda_fixed = NULL
+refresh = FALSE
+# go into robyn_mmm() line by line
