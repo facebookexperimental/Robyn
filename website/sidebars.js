182753f4f02cf50f33cc0f45217b5bf8edfe3daa
Leonel Sentana <leonelsentana@fb.com>
2021-12-16 17:57:54 +0100

    Adding analysts guide to MMM playbook to website

--- website/sidebars.js
+++ website/sidebars.js
@@ -11,6 +11,7 @@ module.exports = {
   someSidebar: {
     'How-to guide': ['quick-start', 'demo-R-script', 'step-by-step-videos'],
     'Robyn at a glance':['robyn-at-a-glance', 'example-plots'],
+    'Analysts guide to MMM':['analysts-guide-to-MMM'],
     Features: [
       'ridge-regression',
       'variable-transformations',
