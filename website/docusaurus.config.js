9df60cf44971224458acbca8a2c4e03cd2c44d29
kyletgoldberg <84025306+kyletgoldberg@users.noreply.github.com>
2022-03-23 13:22:58 -0400

    shift ga and gtag to presets
    necessary docusaurus change https://github.com/facebook/docusaurus/pull/5832

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -106,14 +106,6 @@ module.exports = {
       // Please do not remove the credits, help to publicize Docusaurus :)
       copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc. Built with Docusaurus.`,
     },
-    googleAnalytics: {
-      trackingID: 'G-TMZK4YFDGL',
-      anonymizeIP: true,
-    },
-    gtag: {
-      trackingID: 'G-TMZK4YFDGL',
-      anonymizeIP: true,
-    },
   },
   presets: [
     [
@@ -127,6 +119,14 @@ module.exports = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-TMZK4YFDGL',
+          anonymizeIP: true,
+        },
+        gtag: {
+          trackingID: 'G-TMZK4YFDGL',
+          anonymizeIP: true,
+        },      
       },
     ],
   ],
