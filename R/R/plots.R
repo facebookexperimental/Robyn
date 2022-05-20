0f4f42d5eb238344d58ea7e3ec9992a7b7cd2bfd
Bernardo Lares <bernardolares@fb.com>
2022-05-20 09:40:18 -0500

    fix: inverted labels on weibull adstock curves

--- R/R/plots.R
+++ R/R/plots.R
@@ -356,7 +356,7 @@ robyn_onepagers <- function(InputCollect, OutputCollect, select_model = NULL, qu
           theme_lares(legend = "none", grid = "Xx") +
           labs(
             title = paste0("Weibull Adstock ", wb_type, ": Flexible Decay Rate Over Time"),
-            x = sprintf("Time unit [%ss]", InputCollect$intervalType), y = NULL
+            y = sprintf("Time unit [%ss]", InputCollect$intervalType), x = NULL
           )
       }
 
