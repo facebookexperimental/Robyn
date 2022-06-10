555ad3a27017c8afb2216c1420316e3c68a07d21
Sinan Djevdet <sinandjevdet@fb.com>
2022-06-10 15:26:40 +0100

    added logger and fixed previous date issue, currently a data table error

--- util/robyn_v02.py
+++ util/robyn_v02.py
@@ -94,7 +94,7 @@ try:
     r.data('dt_simulated_weekly')
     r['dt_simulated_weekly'].head()
 
-    # Import data then convert to R data frame
+    # Import data then convert to R data frame ###
 
     def get_file(path:str):
         return sorted(pathlib.Path('.').glob(f'**/{path}'))[0]
