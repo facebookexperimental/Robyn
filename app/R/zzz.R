66bb488e563c623f2b89131b22a3ba4179b579ee
laresbernardo <laresbernardo@gmail.com>
2022-09-12 14:39:43 -0500

    feat: RobynLearn startup Message

---
+++ app/R/zzz.R
@@ -0,0 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(paste(
+      ">>> Welcome to RobynLearn. To load the interactive app, run: robyn_learn()"
+  ))
+}
