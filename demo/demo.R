70304f2336942462c7cd86be5c37e6cd488ba1cf
gufengzhou <gufengzhou@gmail.com>
2023-03-29 08:34:26 +0800

    doc: revert default iteration

--- demo/demo.R
+++ demo/demo.R
@@ -302,8 +302,8 @@ if (length(InputCollect$exposure_vars) > 0) {
 OutputModels <- robyn_run(
   InputCollect = InputCollect, # feed in all model specification
   cores = NULL, # NULL defaults to (max available - 1)
-  iterations = 1000, # 2000 recommended for the dummy dataset with no calibration
-  trials = 1, # 5 recommended for the dummy dataset
+  iterations = 2000, # 2000 recommended for the dummy dataset with no calibration
+  trials = 5, # 5 recommended for the dummy dataset
   ts_validation = TRUE, # 3-way-split time series for NRMSE validation.
   add_penalty_factor = FALSE # Experimental feature. Use with caution.
 )
