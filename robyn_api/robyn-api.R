addd980c32729554828ae4ee918619d878d4bf5e
yu-ya-tanaka <yuyatanaka@meta.com>
2023-10-16 23:28:36 +0900

    feat: plumber for robyn_output and robyn_onepagers
    - Added endpoint of robyn_output and robyn_onepagers

--- robyn_api/robyn-api.R
+++ robyn_api/robyn-api.R
@@ -66,16 +66,155 @@ convert_dates_to_Date <- function(json_data) {
   return(converted_data)
 }
 
-#* Fetch prophet data
+#* Fetch demo data
 #* @post /dt_simulated_weekly
 function() {
   return(Robyn::dt_simulated_weekly)
 }
 
-#* Fetch prophet data
+#* Fetch holiday data
 #* @post /dt_prophet_holidays
 function() {
   return(Robyn::dt_prophet_holidays)
+}
+
+#* Input parameters, hyperparameters, and datasets for models and post back InputCollects
+#* @param modelData Model data feather file in hex format
+#* @param holidayData Holiday data feather file in hex format
+#* @param jsonInput Additional parameters for robyninputs in json format
+#* @post /robyn_inputs
+function(modelData, holidayData, jsonInput) {
+
+  dt_input_bytes <- hex_to_raw(modelData)
+  dt_input <- arrow::read_feather(dt_input_bytes)
+
+  dt_holiday_bytes <- hex_to_raw(holidayData)
+  dt_holiday <- arrow::read_feather(dt_holiday_bytes)
+
+  argsInp <- jsonlite::fromJSON(jsonInput)
+
+  InputCollect <- robyn_inputs(dt_input = dt_input,
+                               dt_holidays = dt_prophet_holidays,
+                               json_file = argsInp
+  )
+
+  return(recursive_ggplot_serialize(InputCollect))
+
+}
+
+#* Run a model and post back output models
+#* @param InputCollect
+#* @param jsonRunArgs Additional parameters for robynrun in json format
+#* @post /robyn_run
+function(InputCollect, jsonRunArgs) {
+
+  InputCollect <- jsonlite::fromJSON(InputCollect) %>% convert_dates_to_Date()
+  # String > Date
+  InputCollect <- convert_dates_to_Date(InputCollect)
+  # list > tibble
+  vars_to_tibble <- c("dt_input", "dt_holidays", "dt_mod", "dt_modRollWind", "dt_inputRollWind")
+  for (var in vars_to_tibble) {
+    InputCollect[[var]] <- as_tibble(InputCollect_fromAPI[[var]])
+  }
+  # Null Treatment
+  for (var in names(InputCollect)) {
+    if(length(InputCollect[[var]])==0) {
+      InputCollect[[var]] <- NULL
+    }
+  }
+
+  argsRun <- jsonlite::fromJSON(jsonRunArgs)
+
+  OutputModels <- do.call(robyn_run, c(list(InputCollect = InputCollect), argsRun))
+
+  return(recursive_ggplot_serialize(OutputModels))
+
+}
+
+
+#* Run a model selection and post back output collect
+#* @param InputCollect
+#* @param OutputModels
+#* @param jsonRunArgs Additional parameters for robyn_output in json format
+#* @post /robyn_outputs
+function(InputCollect, OutputModels, jsonRunArgs, onePagers=FALSE) {
+
+  InputCollect <- jsonlite::fromJSON(InputCollect) %>% convert_dates_to_Date()
+  # String > Date
+  InputCollect <- convert_dates_to_Date(InputCollect)
+  # list > tibble
+  vars_to_tibble <- c("dt_input", "dt_holidays", "dt_mod", "dt_modRollWind", "dt_inputRollWind")
+  for (var in vars_to_tibble) {
+    InputCollect[[var]] <- as_tibble(InputCollect_fromAPI[[var]])
+  }
+  # Null Treatment
+  for (var in names(InputCollect)) {
+    if(length(InputCollect[[var]])==0) {
+      InputCollect[[var]] <- NULL
+    }
+  }
+
+  OutputModels <- jsonlite::fromJSON(OutputModels)
+
+  argsRun <- jsonlite::fromJSON(jsonRunArgs)
+
+  OutputCollect <- do.call(robyn_outputs, c(list(InputCollect = InputCollect, OutputModels = OutputModels), argsRun))
+
+  return(recursive_ggplot_serialize(OutputCollect))
+
+}
+
+#* Call back onepager
+#* @param InputCollect
+#* @param OutputCollect
+#* @param select_model
+#* @param dpi
+#* @param width
+#* @param height
+#* @post /robyn_onepagers
+function(InputCollect, OutputCollect, select_model, dpi=900, width=17, height=19) {
+
+  InputCollect <- jsonlite::fromJSON(InputCollect) %>% convert_dates_to_Date()
+  # String > Date
+  InputCollect <- convert_dates_to_Date(InputCollect)
+  # list > tibble
+  vars_to_tibble <- c("dt_input", "dt_holidays", "dt_mod", "dt_modRollWind", "dt_inputRollWind")
+  for (var in vars_to_tibble) {
+    InputCollect[[var]] <- as_tibble(InputCollect_fromAPI[[var]])
+  }
+  # Null Treatment
+  for (var in names(InputCollect)) {
+    if(length(InputCollect[[var]])==0) {
+      InputCollect[[var]] <- NULL
+    }
+  }
+
+  OutputCollect <- jsonlite::fromJSON(OutputCollect)
+  class(OutputCollect) <- c("robyn_outputs", "list")
+
+  onepager <- robyn_onepagers(InputCollect, OutputCollect, select_model = select_model, export = FALSE)
+
+  return(recursive_ggplot_serialize(onepager, dpi=900, width=17, height=19))
+
+}
+
+#* Create and post back a .zip of Robyn_YYYYMMDDHHMM_init/
+#* @serializer contentType list(type="application/zip")
+#* @get /download_outputs_folder
+function(res) {
+  # Call the function to create the ZIP file
+  zip_file_path <- '/Users/yuyatanaka/Downloads/test.zip'
+
+  # Read the ZIP file as binary
+  zip_content <- readBin(zip_file_path, "raw", file.info(zip_file_path)$size)
+
+  # Set the response type to application/zip
+  res$setHeader("Content-Type", "application/zip")
+  # Set the filename for the downloaded ZIP file
+  res$setHeader("Content-Disposition", paste("attachment; filename=downloaded_file.zip"))
+
+  # Return the ZIP content
+  as.raw(zip_content)
 }
 
 
@@ -115,104 +254,3 @@ function() {
 #   return(recursive_ggplot_serialize(OutputCollect))
 #
 # }
-
-#* Input parameters, hyperparameters, and datasets for models and post back InputCollects
-#* @param modelData Model data feather file in hex format
-#* @param holidayData Holiday data feather file in hex format
-#* @param jsonInput Additional parameters for robyninputs in json format
-#* @post /robyn_inputs
-function(modelData, holidayData, jsonInput) {
-
-  dt_input_bytes <- hex_to_raw(modelData)
-  dt_input <- arrow::read_feather(dt_input_bytes)
-
-  dt_holiday_bytes <- hex_to_raw(holidayData)
-  dt_holiday <- arrow::read_feather(dt_holiday_bytes)
-
-  argsInp <- jsonlite::fromJSON(jsonInput)
-
-  InputCollect <- robyn_inputs(dt_input = dt_input,
-                               dt_holidays = dt_prophet_holidays,
-                               json_file = argsInp
-  )
-
-  return(recursive_ggplot_serialize(InputCollect))
-
-}
-
-#* Run a model and post back output models
-#* @param InputCollect
-#* @param jsonRunArgs Additional parameters for robynrun in json format
-#* @post /robyn_run
-function(InputCollect, jsonRunArgs) {
-
-  InputCollect <- jsonlite::fromJSON(InputCollect) %>% convert_dates_to_Date()
-  InputCollect$dt_input  <- InputCollect$dt_input %>% as_tibble()
-  InputCollect$dt_holidays  <- InputCollect$dt_holidays %>% as_tibble()
-  InputCollect$dt_mod  <- InputCollect$dt_mod %>% as_tibble()
-  InputCollect$dt_modRollWind  <- InputCollect$dt_modRollWind %>% as_tibble()
-  InputCollect$dt_inputRollWind  <- InputCollect$dt_inputRollWind %>% as_tibble()
-  if(length(InputCollect$calibration_input)==0) {InputCollect$calibration_input <- NULL}
-
-  argsRun <- jsonlite::fromJSON(jsonRunArgs)
-
-  OutputModels <- do.call(robyn_run, c(list(InputCollect = InputCollect), argsRun))
-
-  return(recursive_ggplot_serialize(OutputModels))
-
-}
-
-
-#* Run a model selection and post back output collect
-#* @param InputCollect
-#* @param OutputModels
-#* @param jsonRunArgs Additional parameters for robyn_output in json format
-#* @post /robyn_outputs
-function(InputCollect, OutputModels, jsonRunArgs) {
-
-  InputCollect <- jsonlite::fromJSON(InputCollect) %>% convert_dates_to_Date()
-  InputCollect$dt_input  <- InputCollect$dt_input %>% as_tibble()
-  InputCollect$dt_holidays  <- InputCollect$dt_holidays %>% as_tibble()
-  InputCollect$dt_mod  <- InputCollect$dt_mod %>% as_tibble()
-  InputCollect$dt_modRollWind  <- InputCollect$dt_modRollWind %>% as_tibble()
-  InputCollect$dt_inputRollWind  <- InputCollect$dt_inputRollWind %>% as_tibble()
-  if(length(InputCollect$calibration_input)==0) {InputCollect$calibration_input <- NULL}
-
-  OutputModels <- jsonlite::fromJSON(OutputModels)
-  # OutputModels$trial1$resultCollect$resultHypParam <- OutputModels$trial1$resultCollect$resultHypParam %>% as_tibble()
-  # OutputModels$trial1$resultCollect$xDecompAgg <- OutputModels$trial1$resultCollect$xDecompAgg %>% as_tibble()
-
-  argsRun <- jsonlite::fromJSON(jsonRunArgs)
-
-  OutputCollect <- do.call(robyn_outputs, c(list(InputCollect = InputCollect, OutputModels = OutputModels), argsRun))
-
-  if(onePagers){
-    one_pagers <- list()
-    for (select_model in OutputCollect$clusters$models$solID) {
-      one_pagers[[select_model]] <- recursive_ggplot_serialize(robyn_onepagers(InputCollect, OutputCollect, select_model = select_model, export = FALSE),dpi=900,width=17,height=19)
-    }
-    OutputCollect$clusters$models$onepagers <- one_pagers
-  }
-
-  return(recursive_ggplot_serialize(OutputCollect))
-
-}
-
-#* Create and post back a .zip of Robyn_YYYYMMDDHHMM_init/
-#* @serializer contentType list(type="application/zip")
-#* @get /download_outputs_folder
-function(res) {
-  # Call the function to create the ZIP file
-  zip_file_path <- '/Users/yuyatanaka/Downloads/test.zip'
-
-  # Read the ZIP file as binary
-  zip_content <- readBin(zip_file_path, "raw", file.info(zip_file_path)$size)
-
-  # Set the response type to application/zip
-  res$setHeader("Content-Type", "application/zip")
-  # Set the filename for the downloaded ZIP file
-  res$setHeader("Content-Disposition", paste("attachment; filename=downloaded_file.zip"))
-
-  # Return the ZIP content
-  as.raw(zip_content)
-}
