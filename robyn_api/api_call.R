125b3b4f68a502bbe1ca3fefb48e83bd0ef2066b
ebyjose <ebyjose@outlook.com>
2023-10-20 15:16:09 +1100

    Update path detection on API and Instructions on jupyter
    1. Updated api_call.R to fetch working directory and navigate. This is to ensure that the file does not end up not firing with env or path issues
    2. Updated Jupyter notebook with commands for windows/Linux and Mac.
    3. Updated Jupyter notebook with first iteration of Table of contents

--- robyn_api/api_call.R
+++ robyn_api/api_call.R
@@ -1,3 +1,30 @@
 
 library(plumber)
-r<-plumb("~/GitHub/robyn/robyn_api/robyn-api.R")$run(port=9999)
+
+##This function finds the directory where this script is hosted
+stub <- function() {}
+thisPath <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  if (length(grep("^-f$", cmdArgs)) > 0) {
+    # R console option
+    normalizePath(dirname(cmdArgs[grep("^-f", cmdArgs) + 1]))[1]
+  } else if (length(grep("^--file=", cmdArgs)) > 0) {
+    # Rscript/R console option
+    scriptPath <- normalizePath(dirname(sub("^--file=", "", cmdArgs[grep("^--file=", cmdArgs)])))[1]
+  } else if (Sys.getenv("RSTUDIO") == "1") {
+    # RStudio
+    dirname(rstudioapi::getSourceEditorContext()$path)
+  } else if (is.null(attr(stub, "srcref")) == FALSE) {
+    # 'source'd via R console
+    dirname(normalizePath(attr(attr(stub, "srcref"), "srcfile")$filename))
+  } else {
+    stop("Cannot find file path")
+  }
+}
+
+path2file <- thisPath()
+apiPath <- paste0(path2file,'/robyn-api.R')
+
+
+### Command to run Robyn API
+r<-plumb(apiPath)$run(port=9999)
