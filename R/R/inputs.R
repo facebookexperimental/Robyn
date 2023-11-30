e8a449ec1b0a661f67867230bd047c3191c8e387
laresbernardo <laresbernardo@gmail.com>
2023-11-30 20:55:35 +0100

    fix: InputCollect print when prophet is deactivated

--- R/R/inputs.R
+++ R/R/inputs.R
@@ -423,7 +423,7 @@ Adstock: {x$adstock}
     range = paste(range(x$dt_input[, x$date_var][[1]]), collapse = ":"),
     windows = paste(x$window_start, x$window_end, sep = ":"),
     custom_params = if (length(x$custom_params) > 0) paste("\n", flatten_hyps(x$custom_params)) else "None",
-    prophet = if (!is.null(x$prophet_vars)) {
+    prophet = if (length(x$prophet_vars) > 0) {
       sprintf("%s on %s", paste(x$prophet_vars, collapse = ", "), x$prophet_country)
     } else {
       "\033[0;31mDeactivated\033[0m"
