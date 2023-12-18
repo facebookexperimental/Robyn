88c9fb49b9e56f6795b42be1495777b852e73e9c
laresbernardo <laresbernardo@gmail.com>
2023-12-18 14:18:12 +0100

    fix: improved performance no order

--- R/R/model.R
+++ R/R/model.R
@@ -897,8 +897,7 @@ robyn_mmm <- function(InputCollect,
               registerDoSEQ()
             }
             suppressPackageStartupMessages(
-              doparCollect <- foreach(i = 1:iterPar, .inorder = TRUE, ...) %dorng%
-                force(robyn_iterations(i))
+              doparCollect <- foreach(i = 1:iterPar, ...) %dorng% robyn_iterations(i)
             )
           }
 
