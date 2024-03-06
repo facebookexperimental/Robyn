39754be9d9455f3b621a9ed6dc0108ebe77ce3a3
Dhaval Patel <funny@fb.com>
2024-03-06 10:21:40 -0600

    added mapping of R object to python for inputcollect

--- python/tests/inputs_test.py
+++ python/tests/inputs_test.py
@@ -1,12 +1,14 @@
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# # (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
 
-import unittest
+# import unittest
+# import tests.outputs_test
 
+# if __name__ == '__main__':
+#     unittest.main()
+# # class InputsTest(unittest.TestCase):
+# #     def setUp(self) -> None:
+# #         super().setUp()
 
-class InputsTest(unittest.TestCase):
-    def setUp(self) -> None:
-        super().setUp()
-
-    def test_DO_NOT_COMMIT(self) -> None:
-        pass
+# #     def test_DO_NOT_COMMIT(self) -> None:
+# #         pass
