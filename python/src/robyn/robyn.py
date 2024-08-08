227ea1eb0c14a68a4334905039fd664521083010
Sandeep Mane <sandeepongtalk@gmail.com>
2024-08-08 13:17:21 -0700

    Updated robyn abstract APIs

--- python/src/robyn/robyn.py
+++ python/src/robyn/robyn.py
@@ -1,7 +1,7 @@
 # pyre-strict
 
-
-#This needs to be rewritten to match the new structure of the codebase
+# TODO This needs to be rewritten to match the new structure of the codebase
+# TODO Add separate methods if state is loaded from robyn_object or json_file for each method
 
 from robyn.analysis.budget_allocation_result import BudgetAllocationResult
 from robyn.analysis.budgetallocator_config import BudgetAllocatorConfig
@@ -19,6 +19,8 @@ class Robyn:
             working_dir (str): The path to the working directory.
         """
         self.working_dir = working_dir
+        self.mmm_data_collection: MMMDataCollection = None
+        self.model_output_collection: ModelOutputCollection = None
 
     # Load input data for the first time and validates
     def initialize(
@@ -27,7 +29,7 @@ class Robyn:
         holidays_data: HolidaysData,
         hyperparameters: HyperParametersConfig,
         calibration_input: CalibrationInputConfig,
-    ) -> MMMDataCollection:
+    ) -> None:
         """
         Loads input data for the first time and validates it.
 
@@ -40,12 +42,12 @@ class Robyn:
         pass
 
     # Load previous state from Json file
-    def reload_from_json(self, mmmdata_collection_json_file: str) -> None:
+    def reinitialize_from_json(self, robyn_object_json_file: str) -> None:
         """
         Loads the previous state from a JSON file and validates it.
 
         Args:
-            mmmdata_collection_json_file (str): The path to the JSON file containing the previous state.
+            robyn_object_json_file (str): The path to the JSON file containing the previous state.
         """
         pass
 
@@ -88,28 +90,8 @@ class Robyn:
         """
         pass
 
-    # Run budget allocator for given robyn_object?
-    def budget_allocator(
-        self,
-        robyn_object: object,
-        select_model: str,
-        budger_allocator_config: BudgetAllocatorConfig,
-    ) -> BudgetAllocationResult:
-        """
-        Runs the budget allocator for the given robyn_object.
-
-        Args:
-            robyn_object (object): The robyn object to use for budget allocation.
-            select_model (str): The selected model to use for budget allocation.
-            budger_allocator_config (BudgetAllocatorConfig): The budget allocator configuration object.
-
-        Returns:
-            BudgetAllocationResult: The budget allocation result object.
-        """
-        pass
-
     # Run budget allocator - load from json file? # Using json file from robyn_write() for allocation
-    def budget_allocator(
+    def budget_allocator_from_json(
         self,
         robyn_object_json: str,
         select_model: str,
@@ -131,20 +113,14 @@ class Robyn:
     # Refresh
     def model_refresh(
         self,
-        mmmdata_colllection: MMMDataCollection,
-        model_output_collection: ModelOutputsCollection,
         refresh_config: ModelRefreshConfig,
-        calibration_input: CalibrationInputConfig = None,
         objective_weights=None,
     ) -> BudgetAllocationResult:
         """
         Refreshes the model using the specified configuration and input.
 
         Args:
-            mmmdata_colllection (MMMDataCollection): The MMM data collection object.
-            model_output_collection (ModelOutputsCollection): The model output collection object.
             refresh_config (ModelRefreshConfig): The refresh configuration object.
-            calibration_input (CalibrationInputConfig, optional): The calibration input configuration object. Defaults to None.
             objective_weights (dict, optional): The objective weights dictionary. Defaults to None.
 
         Returns:
@@ -152,14 +128,7 @@ class Robyn:
         """
         pass
 
-    # def model_refresh(self, robyn_object? -> ?, refresh_config:ModelRefreshConfig, calibration_input:CalibrationInputConfig=None, objective_weights=None) -> ? :
-    #         pass
-    # def model_refresh(seld, json_file -> ?, refresh_config:ModelRefreshConfig, calibration_input:CalibrationInputConfig=None, objective_weights=None) -> ? :
-    #     pass
-
     def robyn_response(
-        mmm_data_collection: MMMDataCollection = None,
-        model_output_collection: ModelOutputCollection = None,
         select_build=None,
         select_model=None,
         metric_name=None,
@@ -173,8 +142,6 @@ class Robyn:
         Generates a response for the given input parameters.
 
         Args:
-            mmm_data_collection (MMMDataCollection, optional): The MMM data collection object. Defaults to None.
-            model_output_collection (ModelOutputCollection, optional): The model output collection object. Defaults to None.
             select_build (_type_, optional): The selected build. Defaults to None.
             select_model (_type_, optional): The selected model. Defaults to None.
             metric_name (_type_, optional): The metric name. Defaults to None.
@@ -182,11 +149,11 @@ class Robyn:
             date_range (_type_, optional): The date range. Defaults to None.
             dt_hyppar (_type_, optional): The dt hyppar. Defaults to None.
             dt_coef (_type_, optional): The dt coef. Defaults to None.
-            quiet (bool, optional): Whether to suppress output. Defaults to False.
         """
         pass
 
-    def robyn_response(
+    # If json_file has everything always including MMMDataCollection, ModelOutputCollection then use reinitialize_from_json
+    def robyn_response_from_json(
         json_file=None,
         select_build=None,
         select_model=None,
@@ -195,7 +162,6 @@ class Robyn:
         date_range=None,
         dt_hyppar=None,
         dt_coef=None,
-        quiet=False,
     ):
         """
         Generates a response for the given input parameters using a JSON file.
@@ -209,33 +175,5 @@ class Robyn:
             date_range (_type_, optional): The date range. Defaults to None.
             dt_hyppar (_type_, optional): The dt hyppar. Defaults to None.
             dt_coef (_type_, optional): The dt coef. Defaults to None.
-            quiet (bool, optional): Whether to suppress output. Defaults to False.
-        """
-        pass
-
-    def robyn_response(
-        robyn_object=None,
-        select_build=None,
-        select_model=None,
-        metric_name=None,
-        metric_value=None,
-        date_range=None,
-        dt_hyppar=None,
-        dt_coef=None,
-        quiet=False,
-    ):
-        """
-        Generates a response for the given input parameters using a Robyn object.
-
-        Args:
-            robyn_object (_type_, optional): The Robyn object. Defaults to None.
-            select_build (_type_, optional): The selected build. Defaults to None.
-            select_model (_type_, optional): The selected model. Defaults to None.
-            metric_name (_type_, optional): The metric name. Defaults to None.
-            metric_value (_type_, optional): The metric value. Defaults to None.
-            date_range (_type_, optional): The date range. Defaults to None.
-            dt_hyppar (_type_, optional): The dt hyppar. Defaults to None.
-            dt_coef (_type_, optional): The dt coef. Defaults to None.
-            quiet (bool, optional): Whether to suppress output. Defaults to False.
         """
         pass
