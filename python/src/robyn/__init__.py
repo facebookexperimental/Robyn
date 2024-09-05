5560a316dea0978bb787e4e43ee89f3b50ab5a02
Sandeep Mane <sandeepongtalk@gmail.com>
2024-09-04 22:16:11 -0700

    Fixed logger and tested.

--- python/src/robyn/__init__.py
+++ python/src/robyn/__init__.py
@@ -1,7 +1,13 @@
 import logging.config
 import datetime
+import os
 
 # Get the current datetime
 current_datetime = datetime.datetime.now()
 
-logging.config.fileConfig('logging.conf', defaults={'asctime': current_datetime.strftime('%Y-%m-%d_%H-%M-%S')})
+#print(os.getcwd())
+
+logging_conf_path = os.path.join(os.path.dirname(__file__), 'common/config/logging.conf')
+
+# Load the logging configuration file
+logging.config.fileConfig(logging_conf_path, defaults={'asctime': current_datetime.strftime('%Y-%m-%d_%H-%M-%S')})
