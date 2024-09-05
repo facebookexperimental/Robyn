0e98d3053a0411d0a5c88a795140f552f4dcfebc
Sandeep Mane <sandeepongtalk@gmail.com>
2024-09-04 22:29:58 -0700

    Minor updates to __init__.py

--- python/src/robyn/__init__.py
+++ python/src/robyn/__init__.py
@@ -1,13 +1,16 @@
-import logging.config
 import datetime
+import logging.config
 import os
 
 # Get the current datetime
 current_datetime = datetime.datetime.now()
 
-#print(os.getcwd())
-
-logging_conf_path = os.path.join(os.path.dirname(__file__), 'common/config/logging.conf')
+logging_conf_path = os.path.join(
+    os.path.dirname(__file__), "common/config/logging.conf"
+)
 
 # Load the logging configuration file
-logging.config.fileConfig(logging_conf_path, defaults={'asctime': current_datetime.strftime('%Y-%m-%d_%H-%M-%S')})
+logging.config.fileConfig(
+    logging_conf_path,
+    defaults={"asctime": current_datetime.strftime("%Y-%m-%d_%H-%M-%S")},
+)
