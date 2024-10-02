5062f62b86326dd0f3b0c84e658a4e2c8bbdc8a6
alxlyj <yijuilee@meta.com>
2024-10-01 22:18:48 -0400

    update convergence code

--- python/src/robyn/modeling/entities/modeloutputs.py
+++ python/src/robyn/modeling/entities/modeloutputs.py
@@ -7,19 +7,6 @@ import pandas as pd
 
 @dataclass
 class Trial:
-    """
-    Represents the results of a single model trial.
-
-    Attributes:
-        result_hyp_param (pd.DataFrame): Hyperparameters and their values for this trial.
-        x_decomp_agg (pd.DataFrame): Aggregated decomposition results for this trial.
-        lift_calibration (pd.DataFrame): Lift calibration results for this trial.
-        decomp_spend_dist (pd.DataFrame): Decomposition spend distribution for this trial.
-        nrmse (float): Normalized Root Mean Square Error for this trial.
-        decomp_rssd (float): Decomposition Root Sum Squared Distance for this trial.
-        mape (float): Mean Absolute Percentage Error for this trial.
-    """
-
     result_hyp_param: pd.DataFrame
     lift_calibration: Optional[pd.DataFrame]
     decomp_spend_dist: Optional[pd.DataFrame]
@@ -37,7 +24,6 @@ class Trial:
     pos: int
     elapsed: float
     elapsed_accum: float
-    sol_id: str
     trial: int
     iter_ng: int
     iter_par: int
@@ -65,7 +51,6 @@ class ModelOutputs:
         hyper_updated (Dict[str, Any]): Updated hyperparameters.
         hyper_fixed (bool): Whether hyperparameters were fixed.
         convergence (Dict[str, Any]): Convergence information for the optimization process.
-        ts_validation_plot (Any): Time series validation plot (if applicable).
         select_id (str): ID of the selected model.
         seed (int): Random seed used for reproducibility.
     """
@@ -82,7 +67,6 @@ class ModelOutputs:
     hyper_updated: Dict[str, Any]
     hyper_fixed: bool
     convergence: Dict[str, Any]
-    ts_validation_plot: Any  # This could be a matplotlib figure or other plot object
     select_id: str
     seed: int
     hyper_bound_ng: Dict[str, Any]  # For hyperBoundNG
