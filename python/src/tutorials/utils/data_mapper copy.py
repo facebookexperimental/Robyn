440c667d8077de8932d307e09c5aa00376742192
alxlyj <yijuilee@meta.com>
2024-10-04 13:03:31 -0400

    update data mapper code

--- python/src/tutorials/utils/data_mapper copy.py
+++ python/src/tutorials/utils/data_mapper copy.py
@@ -1,4 +1,3 @@
-# data_mapper.py
 import json
 import pandas as pd
 from typing import Dict, Any
@@ -19,7 +18,6 @@ from robyn.data.entities.enums import (
     ProphetSigns,
     CalibrationScope,
 )
-from robyn.modeling.entities.convergence import Convergence
 
 
 def export_data(
@@ -77,50 +75,55 @@ def import_data(data: Dict[str, Any]) -> Dict[str, Any]:
         modNLS=data["InputCollect"].get("modNLS", {}),
     )
     # Other initializations...
-    # Process OutputModels
     trials = []
-    attributes = {}
-    convergence_data = None
-
+    attributes = {}  # Initialize an empty dictionary to store attributes
+    print("Keys of OutputModels:", data["OutputModels"].keys())
     for trial_key, trial_data in data["OutputModels"].items():
-        if trial_key == "attributes":
-            attributes = trial_data
-        elif trial_key == "convergence":
-            convergence_data = Convergence.from_dict(trial_data)
-        elif isinstance(trial_data, dict):
+        print("====================================")
+        print("Name of trial key:", trial_key)
+        print("Type of trial key:", type(trial_key))
+        try:
+            print("Type of trial data:", type(trial_data))
+            print("Keys of trial data:", trial_data.keys())
+        except:
+            print("Not a dictionary")
+        if isinstance(trial_data, dict):
+            # This is a trial entry
             result_collect = trial_data.get("resultCollect", {})
             result_hyp_param = pd.DataFrame(result_collect.get("resultHypParam", []))
+            print("Result Hyp Param:", result_hyp_param)
             x_decomp_agg = pd.DataFrame(result_collect.get("xDecompAgg", []))
             lift_calibration = pd.DataFrame(result_collect.get("liftCalibration", []))
             decomp_spend_dist = pd.DataFrame(result_collect.get("decompSpendDist", []))
-
-            for _, row in result_hyp_param.iterrows():
-                trial = Trial(
-                    result_hyp_param=result_hyp_param,
-                    x_decomp_agg=x_decomp_agg,
-                    lift_calibration=lift_calibration,
-                    decomp_spend_dist=decomp_spend_dist,
-                    nrmse=row.get("NRMSE", 0),
-                    decomp_rssd=row.get("decomp.rssd", 0),
-                    mape=row.get("MAPE", 0),
-                    rsq_train=row.get("rsq_train", 0),
-                    rsq_val=row.get("rsq_val", 0),
-                    rsq_test=row.get("rsq_test", 0),
-                    lambda_=row.get("lambda", 0),
-                    lambda_hp=row.get("lambda_hp", 0),
-                    lambda_max=row.get("lambda_max", 0),
-                    lambda_min_ratio=row.get("lambda_min_ratio", 0),
-                    pos=row.get("pos", 0),
-                    elapsed=row.get("Elapsed", 0),
-                    elapsed_accum=row.get("ElapsedAccum", 0),
-                    trial=row.get("trial", 0),
-                    iter_ng=row.get("iterNG", 0),
-                    iter_par=row.get("iterPar", 0),
-                    train_size=row.get("train_size", 0),
-                    sol_id=row.get("solID", ""),
-                )
-                trials.append(trial)
-
+            first_hyp_param = result_hyp_param.iloc[0] if not result_hyp_param.empty else {}
+            trial = Trial(
+                result_hyp_param=result_hyp_param,
+                x_decomp_agg=x_decomp_agg,
+                lift_calibration=lift_calibration,
+                decomp_spend_dist=decomp_spend_dist,
+                nrmse=first_hyp_param.get("NRMSE", 0),
+                decomp_rssd=first_hyp_param.get("RSSD", 0),
+                mape=first_hyp_param.get("MAPE", 0),
+                rsq_train=first_hyp_param.get("rsq_train", 0),
+                rsq_val=first_hyp_param.get("rsq_val", 0),
+                rsq_test=first_hyp_param.get("rsq_test", 0),
+                lambda_=first_hyp_param.get("lambda", 0),
+                lambda_hp=first_hyp_param.get("lambda_hp", 0),
+                lambda_max=first_hyp_param.get("lambda_max", 0),
+                lambda_min_ratio=first_hyp_param.get("lambda_min_ratio", 0),
+                pos=first_hyp_param.get("pos", 0),
+                elapsed=first_hyp_param.get("elapsed", 0),
+                elapsed_accum=first_hyp_param.get("elapsed_accum", 0),
+                trial=first_hyp_param.get("trial", 0),
+                iter_ng=first_hyp_param.get("iterNG", 0),
+                iter_par=first_hyp_param.get("iterPar", 0),
+                train_size=first_hyp_param.get("train_size", 0),
+                sol_id=first_hyp_param.get("solID", ""),
+            )
+            trials.append(trial)
+        else:
+            # This is likely an attribute entry
+            attributes[trial_key] = trial_data
     model_outputs = ModelOutputs(
         trials=trials,
         train_timestamp=attributes.get("train_timestamp", ""),
@@ -132,12 +135,11 @@ def import_data(data: Dict[str, Any]) -> Dict[str, Any]:
         ts_validation=attributes.get("ts_validation", False),
         add_penalty_factor=attributes.get("add_penalty_factor", False),
         hyper_fixed=attributes.get("hyper_fixed", False),
-        hyper_updated=attributes.get("hyper_updated", {}),
-        convergence=convergence_data,
-        select_id=attributes.get("select_id", ""),
-        seed=attributes.get("seed", 0),
+        hyper_updated={},  # Update this if necessary
+        convergence={},  # Update this if necessary
+        select_id="",  # Update this if necessary
+        seed=0,  # Update this if necessary
     )
-
     return {
         "mmm_data": mmm_data,
         "holidays_data": holidays_data,
@@ -185,5 +187,5 @@ if __name__ == "__main__":
     featurized_mmm_data = imported_data["featurized_mmm_data"]
     model_outputs = imported_data["model_outputs"]
 
-    # print(model_outputs.trials)
+    print(model_outputs.trials)
     # You can now compare these with the outputs from your Python/Python script
