49b107b96824312da5205d08346312b38b1fec23
laresbernardo <laresbernardo@gmail.com>
2025-06-23 18:47:13 +0200

    recode: format

--- R/R/allocator.R
+++ R/R/allocator.R
@@ -300,10 +300,11 @@ robyn_allocator <- function(robyn_object = NULL,
       x_hist_carryover = mean(hist_carryover_temp),
       get_sum = FALSE
     )
-    mr <- ifelse(all(mediaSelectedSorted %in% InputCollect$paid_media_spends),
-      resp$mean_response, resp_simulate
-    )
-    initResponseUnit <- c(initResponseUnit, mr)
+    initResponseUnit <- c(initResponseUnit, ifelse(
+      all(mediaSelectedSorted %in% InputCollect$paid_media_spends),
+      resp$mean_response,
+      resp_simulate
+    ))
     initResponseMargUnit <- c(initResponseMargUnit, resp_simulate_plus1 - resp_simulate)
   }
   qa_carryover <- do.call(cbind, qa_carryover) %>% as.data.frame()
